ee265b09635186863bcfba95650adb7326981407
Fabian Kulman <kulmanf@gmail.com>
2018-05-01 16:07:47 +0200

    FkuMode

--- Algorithm.CSharp/fku/src/FkuButtomReversalAlgorithm.cs
+++ Algorithm.CSharp/fku/src/FkuButtomReversalAlgorithm.cs
@@ -6,14 +6,15 @@ namespace QuantConnect.Algorithm.CSharp
 
     public class FkuButtomReversalAlgorithm : QCAlgorithm
     {
+        private const FkuMode Environment = FkuMode.Regression;
+        
         private FkuUniverseSelection _universe = new FkuUniverseSelection();
 
         public override void Initialize()
         {
             SetCash(10000); 
             SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash);   
-            _universe.Initialize(this, LiveMode);
-       
+            _universe.Initialize(this, Environment);
         }
 
         public override void OnData(Slice data)
--- Algorithm.CSharp/fku/src/FkuUniverseSelection.cs
+++ Algorithm.CSharp/fku/src/FkuUniverseSelection.cs
@@ -1,32 +1,41 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
 
 namespace QuantConnect.Algorithm.CSharp
 {
-    public class FkuUniverseSelection
+    internal enum FkuMode
     {
-       
-        public void Initialize(QCAlgorithm algorithm, bool liveMode)
+        InteractiveBrokers,
+        Backtest,
+        Regression
+    }
+
+    internal class FkuUniverseSelection
+    {
+        public void Initialize(QCAlgorithm algorithm, FkuMode fkuMode)
         {
-            if (liveMode)
+            switch (fkuMode)
             {
-                Symbol = QuantConnect.Symbol.Create("FB", SecurityType.Equity, Market.USA);
-                algorithm.SetStartDate(2018, 04, 25); //Set Start Date
-                algorithm.SetEndDate(2018, 04, 28); //Set End Date
-                algorithm.AddEquity("FB", Resolution.Minute);
-            }
-            else
-            {
-                Symbol = QuantConnect.Symbol.Create("SPY", SecurityType.Equity, Market.USA);
-                algorithm.SetStartDate(2013, 10, 07); //Set Start Date
-                algorithm.SetEndDate(2013, 10, 11); //Set End Date
-                algorithm.AddEquity("SPY", Resolution.Minute);
+                case FkuMode.InteractiveBrokers:
+                    Symbol = Symbol.Create("FB", SecurityType.Equity, Market.USA);
+                    algorithm.AddEquity(Symbol, Resolution.Minute);
+                    break;
+                case FkuMode.Backtest:
+                    Symbol = Symbol.Create("FB", SecurityType.Equity, Market.USA);
+                    algorithm.SetStartDate(2018, 04, 25); //Set Start Date
+                    algorithm.SetEndDate(2018, 04, 28); //Set End Date
+                    algorithm.AddEquity(Symbol, Resolution.Minute);
+                    break;
+                case FkuMode.Regression:
+                    Symbol = Symbol.Create("SPY", SecurityType.Equity, Market.USA);
+                    algorithm.SetStartDate(2013, 10, 07); //Set Start Date
+                    algorithm.SetEndDate(2013, 10, 11); //Set End Date
+                    algorithm.AddEquity("SPY", Resolution.Minute);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(fkuMode), fkuMode, null);
             }
         }
-        
-        public Symbol Symbol
-        {
-            get;
-            private set;
-        }
+
+        internal Symbol Symbol { get; private set; }
     }
 }
