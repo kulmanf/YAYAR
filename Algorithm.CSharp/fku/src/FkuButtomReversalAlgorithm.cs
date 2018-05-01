0837cddb67c6cd4e047024d7c6c0f33c802838a6
Fabian Kulman <kulmanf@gmail.com>
2018-05-01 15:43:15 +0200

    FkuUniverseSelection

--- Algorithm.CSharp/fku/src/FkuButtomReversalAlgorithm.cs
+++ Algorithm.CSharp/fku/src/FkuButtomReversalAlgorithm.cs
@@ -6,34 +6,21 @@ namespace QuantConnect.Algorithm.CSharp
 
     public class FkuButtomReversalAlgorithm : QCAlgorithm
     {
-        private Symbol _symbol = QuantConnect.Symbol.Create("FB", SecurityType.Equity, Market.USA);
+        private FkuUniverseSelection _universe = new FkuUniverseSelection();
 
         public override void Initialize()
         {
             SetCash(10000); 
-            SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash);
-            
-            // Regression
-            _symbol = QuantConnect.Symbol.Create("SPY", SecurityType.Equity, Market.USA);
-            SetStartDate(2013, 10, 07);  //Set Start Date
-            SetEndDate(2013, 10, 11);    //Set End Date
-            AddEquity("SPY", Resolution.Minute);
-            
-            // Backtest
-//            SetStartDate(2018, 04, 25);  //Set Start Date
-//            SetEndDate(2018, 04, 28);    //Set End Date
-//            AddEquity("FB", Resolution.Minute);
-            
-            // Live
-            
-            
+            SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash);   
+            _universe.Initialize(this, LiveMode);
+       
         }
 
         public override void OnData(Slice data)
         {
             if (!Portfolio.Invested)
             {
-                SetHoldings(_symbol, 1);
+                SetHoldings(_universe.Symbol, 1);
                 Debug("Purchased Stock - mighty mac monkey face");
             }
         }
---
+++ Algorithm.CSharp/fku/src/FkuUniverseSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class FkuUniverseSelection
+    {
+       
+        public void Initialize(QCAlgorithm algorithm, bool liveMode)
+        {
+            if (liveMode)
+            {
+                Symbol = QuantConnect.Symbol.Create("FB", SecurityType.Equity, Market.USA);
+                algorithm.SetStartDate(2018, 04, 25); //Set Start Date
+                algorithm.SetEndDate(2018, 04, 28); //Set End Date
+                algorithm.AddEquity("FB", Resolution.Minute);
+            }
+            else
+            {
+                Symbol = QuantConnect.Symbol.Create("SPY", SecurityType.Equity, Market.USA);
+                algorithm.SetStartDate(2013, 10, 07); //Set Start Date
+                algorithm.SetEndDate(2013, 10, 11); //Set End Date
+                algorithm.AddEquity("SPY", Resolution.Minute);
+            }
+        }
+        
+        public Symbol Symbol
+        {
+            get;
+            private set;
+        }
+    }
+}
