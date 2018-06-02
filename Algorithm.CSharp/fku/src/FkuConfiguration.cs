201b5926c13a1b33a7ec92b1b74bb5ee288e1b9b
Fabian Kulman <kulmanf@gmail.com>
2018-06-02 08:28:45 +0200

    date configuration

--- Algorithm.CSharp/fku/src/FkuConfiguration.cs
+++ Algorithm.CSharp/fku/src/FkuConfiguration.cs
@@ -5,13 +5,22 @@ namespace QuantConnect.Algorithm.CSharp
     internal static class FkuConfiguration
     {
         public const FkuMode Environment = FkuMode.Regression;
-        
+
         public const decimal WinPercent = 1; // 1%
         public const decimal LosePercent = -0.5m;
+        
+        public const int startYear = 2018;
+        public const int startMonth = 5;
+        public const int startDay = 1;
+
+        public const int endYear = 2018;
+        public const int endMonth = 5;
+        public const int endDay = 31;
     }
 
     internal class FkuTickers
     {
-        public readonly List<string> Tickers = new List<string>{"FB", "MU", "MTCH", "TSLA"};
+        public readonly List<string> Tickers = new List<string> {"FB", "MU", "MTCH", "TSLA"};
+        
     }
 }
--- Algorithm.CSharp/fku/src/FkuUniverse.cs
+++ Algorithm.CSharp/fku/src/FkuUniverse.cs
@@ -14,35 +14,38 @@ namespace QuantConnect.Algorithm.CSharp
     internal class FkuUniverse
     {
         private QCAlgorithm _algorithm;
-        
+
         internal List<Symbol> Symbols { get; private set; }
-        
+
         internal void Initialize(QCAlgorithm algorithm, FkuMode fkuMode)
         {
             _algorithm = algorithm;
             var tickers = new FkuTickers().Tickers;
             var symbols = tickers.Select(ticker => Symbol.Create(ticker, SecurityType.Equity, Market.USA)).ToList();
-         
+
             switch (fkuMode)
             {
                 case FkuMode.InteractiveBrokers:
                     Symbols = symbols;
-                    break;
-                case FkuMode.Backtest:
-                    Symbols = symbols;
-                    algorithm.SetStartDate(2018, 04, 01); //Set Start Date
-                    algorithm.SetEndDate(2018, 04, 30); //Set End Date
+                    algorithm.SetStartDate(
+                        FkuConfiguration.startYear, 
+                        FkuConfiguration.startMonth,
+                        FkuConfiguration.startDay);
+                    algorithm.SetEndDate(
+                        FkuConfiguration.endYear, 
+                        FkuConfiguration.endMonth, 
+                        FkuConfiguration.endDay);
                     break;
                 case FkuMode.Regression:
                     var spy = Symbol.Create("SPY", SecurityType.Equity, Market.USA);
-                    Symbols = new List<Symbol>{spy};
+                    Symbols = new List<Symbol> {spy};
                     algorithm.SetStartDate(2013, 10, 07); //Set Start Date
                     algorithm.SetEndDate(2013, 10, 11); //Set End Date
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-            
+
             Symbols.ForEach(symbol => algorithm.AddEquity(symbol, extendedMarketHours: true));
         }
 
@@ -50,12 +53,11 @@ namespace QuantConnect.Algorithm.CSharp
         {
             Log("Symbols: " + Symbols);
         }
-        
+
         private void Log(string message)
         {
-            var stamp = _algorithm.Time + " [FkuUniverse] "; 
+            var stamp = _algorithm.Time + " [FkuUniverse] ";
             _algorithm.Log(stamp + message);
         }
-
     }
 }
